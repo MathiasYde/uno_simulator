f663bcafd67c639b42560ba37c07486620b9260b
Mathias <mathias.yde@hotmail.dk>
2021-12-06 15:30:51 +0100

    comments

--- src/Card.java
+++ src/Card.java
@@ -13,15 +13,19 @@ public class Card {
 
 	// Validate if card can be put on this card
 	public Boolean Validate(Card card) {
+		// These cards can be put on any other card
 		if (this.value == CardValue.PickColor) { return true; }
 		if (this.value == CardValue.PickColorPlusFour) { return true; }
 
+		// Cards beyond here must match color
 		if (this.color == card.color) { return true; }
 
+		// These cards can be put on any other card with same color
 		if (this.value == CardValue.PlusTwo) { return true; }
 		if (this.value == CardValue.Twist) { return true; }
 		if (this.value == CardValue.Block) { return true; }
 		
+		// These cards must match in color and value
 		if (this.value == card.value) { return true; }
 
 		return false;
